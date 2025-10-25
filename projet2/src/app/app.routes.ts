0e34c3bc152cb8c442a811447d1c055b6481c6fe
louay <louayyzouaouii@gmail.com>
2025-10-25 10:37:47 +0100

    page home

--- projet2/src/app/app.routes.ts
+++ projet2/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import path from 'path';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+{path: 'home', component: Home}
+];
+
+
